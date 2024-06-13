47ecb462a342ee869b453690e2cc60226c63b2d0
DekyyEx <vlad.moor123@mail.ru>
2024-06-13 16:33:39 +0300

    hm2

---
+++ src/hm2/StringMethodPrinter.java
@@ -0,0 +1,16 @@
+package hm2;
+
+import java.lang.reflect.Method;
+
+public class StringMethodPrinter {
+    public static void main(String[] args) {
+        Class<String> stringClass = String.class;
+
+        Method[] methods = stringClass.getDeclaredMethods();
+
+        System.out.println("Методы класса String:");
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
+    }
+}
